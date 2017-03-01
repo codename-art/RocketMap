590bb0cf8e8714a73326b331952137cc02eafd8c
mpw1337 <markus.wersig@gmail.com>
2017-03-01 12:56:37 +0100

    Added function to get Datestring from a timestamp to make code more readable and remove double Code.

--- static/js/map.js
+++ static/js/map.js
@@ -397,6 +397,15 @@ function openMapDirections(lat, lng) { // eslint-disable-line no-unused-vars
     var url = 'https://www.google.com/maps/?daddr=' + lat + ',' + lng
     window.open(url, '_blank')
 }
+//Converts timestamp to readable String
+function getDateStr(t){
+    var dateStr = 'Unknown'
+    if (t){
+        var d = new Date(t)
+        dateStr = `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())} ${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`
+    }
+    return dateStr
+}
 
 // Converts timestamp to readable String
 function getDateStr(t) {
