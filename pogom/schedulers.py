a2ea3565dbd4bd9a7306bfafa68ca3e72a1b8b81
Arengo <PokemonGoMap@outlook.de>
2017-03-30 15:51:13 +0200

    Hi Travis.

--- pogom/schedulers.py
+++ pogom/schedulers.py
@@ -61,7 +61,7 @@ from operator import itemgetter
 from datetime import datetime, timedelta
 from .transform import get_new_coords
 from .models import (hex_bounds, Pokemon, Pokestop, Gym, SpawnPoint,
-                    ScannedLocation, ScanSpawnPoint)
+                     ScannedLocation, ScanSpawnPoint)
 from .utils import now, cur_sec, cellid, date_secs, equi_rect_distance
 from .altitude import get_altitude
 
