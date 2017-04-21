535c8b20ef73893759fcdee4b82ee46734345ffc
mpw1337 <markus.wersig@googlemail.com>
2017-04-21 15:24:16 +0200

    Last modified in Gym Info and Sidebar (#1876)
    * Added last Modified to Gym info.
    
    * Fix Travis
    
    * Fixed Modified
    
    * Added function to get Datestring from a timestamp to make code more readable and remove double Code.
    
    * Fix Travis
    
    * Added moment to scripts
    
    * Used Moment for formatting.
    
    * Changed lastScannedDate to also use moment.
    
    * Fixed Travis Quote
    
    * Fixed Travis undefined error (I think it's hacky, but don't know how to do it)
    
    * Hopefully fix last Travis Error
    
    * fix package,json (don't know if it is really necessary)
    
    * Added moment global in slintrc and removed require and moment entry in package.json
    
    * One comma to break it all
    
    * Fix time formatting

--- static/js/map.js
+++ static/js/map.js
@@ -70,6 +70,7 @@ var genderType = ['♂', '♀', '⚲']
 var unownForm = ['unset', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '?']
 
 
+
 /*
   text place holders:
   <pkm> - pokemon name
