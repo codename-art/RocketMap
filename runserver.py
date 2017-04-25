267db26f95106025a7fcb6f4fad4455438aa8f83
codename-art <120071994>
2017-04-25 22:16:14 +0300

    add proxy  usage for scout

--- runserver.py
+++ runserver.py
@@ -282,6 +282,18 @@ def main():
         t.start()
 
     if args.scout:
+        # Processing proxies if set (load from file, check and overwrite old
+        # args.proxy with new working list)
+        args.proxy = check_proxies(args)
+
+        # Run periodical proxy refresh thread
+        if (args.proxy_file is not None) and (args.proxy_refresh > 0):
+            t = Thread(target=proxies_refresher,
+                       name='proxy-refresh', args=(args,))
+            t.daemon = True
+            t.start()
+        else:
+            log.info('Periodical proxies refresh disabled.')
         scout_init()
 
     config['ROOT_PATH'] = app.root_path
