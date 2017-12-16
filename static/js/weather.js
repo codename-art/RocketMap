69a9c9b4821a9f21de3a34883792ca4e8bf79ceb
codename-art <120071994>
2017-12-16 03:25:34 +0300

    iterate weather cells on frontend

--- static/js/weather.js
+++ static/js/weather.js
@@ -2,15 +2,18 @@
 
 
     initMap();
-    var bermudaTriangle = new google.maps.Polygon({
-        paths: data.vertices,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35
+    data.forEach(function callback(currentValue, index, array) {
+        var cell = new google.maps.Polygon({
+            paths: currentValue.vertices,
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35
+        });
+        bermudaTriangle.setMap(cell);
     });
-    bermudaTriangle.setMap(map);
+
 })
 
 function initMap() { // eslint-disable-line no-unused-vars
