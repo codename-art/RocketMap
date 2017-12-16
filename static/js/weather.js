3d40559b54bea84feb196ec941153470ac8b91c5
codename-art <120071994>
2017-12-16 03:18:17 +0300

    fix js

--- static/js/weather.js
+++ static/js/weather.js
@@ -1,10 +1,126 @@
-initMap();
-var bermudaTriangle = new google.maps.Polygon({
-    paths: data.vertices,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35
-});
-bermudaTriangle.setMap(map);
+;(function () {
+
+
+    initMap();
+    var bermudaTriangle = new google.maps.Polygon({
+        paths: data.vertices,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35
+    });
+    bermudaTriangle.setMap(map);
+})
+
+function initMap() { // eslint-disable-line no-unused-vars
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: Number(getParameterByName('lat')) || centerLat,
+            lng: Number(getParameterByName('lon')) || centerLng
+        },
+        zoom: Number(getParameterByName('zoom')) || Store.get('zoomLevel'),
+        gestureHandling: 'greedy',
+        fullscreenControl: true,
+        streetViewControl: false,
+        mapTypeControl: false,
+        clickableIcons: false,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            position: google.maps.ControlPosition.RIGHT_TOP,
+            mapTypeIds: [
+                google.maps.MapTypeId.ROADMAP,
+                google.maps.MapTypeId.SATELLITE,
+                google.maps.MapTypeId.HYBRID,
+                'nolabels_style',
+                'dark_style',
+                'style_light2',
+                'style_pgo',
+                'dark_style_nl',
+                'style_light2_nl',
+                'style_pgo_nl',
+                'style_pgo_day',
+                'style_pgo_night',
+                'style_pgo_dynamic'
+            ]
+        }
+    })
+
+    // Enable clustering.
+    var clusterOptions = {
+        imagePath: 'static/images/cluster/m',
+        maxZoom: Store.get('maxClusterZoomLevel'),
+        zoomOnClick: Store.get('clusterZoomOnClick'),
+        gridSize: Store.get('clusterGridSize')
+    }
+
+    markerCluster = new MarkerClusterer(map, [], clusterOptions)
+
+    var styleNoLabels = new google.maps.StyledMapType(noLabelsStyle, {
+        name: 'No Labels'
+    })
+    map.mapTypes.set('nolabels_style', styleNoLabels)
+
+    var styleDark = new google.maps.StyledMapType(darkStyle, {
+        name: 'Dark'
+    })
+    map.mapTypes.set('dark_style', styleDark)
+
+    var styleLight2 = new google.maps.StyledMapType(light2Style, {
+        name: 'Light2'
+    })
+    map.mapTypes.set('style_light2', styleLight2)
+
+    var stylePgo = new google.maps.StyledMapType(pGoStyle, {
+        name: 'RocketMap'
+    })
+    map.mapTypes.set('style_pgo', stylePgo)
+
+    var styleDarkNl = new google.maps.StyledMapType(darkStyleNoLabels, {
+        name: 'Dark (No Labels)'
+    })
+    map.mapTypes.set('dark_style_nl', styleDarkNl)
+
+    var styleLight2Nl = new google.maps.StyledMapType(light2StyleNoLabels, {
+        name: 'Light2 (No Labels)'
+    })
+    map.mapTypes.set('style_light2_nl', styleLight2Nl)
+
+    var stylePgoNl = new google.maps.StyledMapType(pGoStyleNoLabels, {
+        name: 'RocketMap (No Labels)'
+    })
+    map.mapTypes.set('style_pgo_nl', stylePgoNl)
+
+    var stylePgoDay = new google.maps.StyledMapType(pGoStyleDay, {
+        name: 'RocketMap Day'
+    })
+    map.mapTypes.set('style_pgo_day', stylePgoDay)
+
+    var stylePgoNight = new google.maps.StyledMapType(pGoStyleNight, {
+        name: 'RocketMap Night'
+    })
+    map.mapTypes.set('style_pgo_night', stylePgoNight)
+
+    // dynamic map style chooses stylePgoDay or stylePgoNight depending on client time
+    var currentDate = new Date()
+    var currentHour = currentDate.getHours()
+    var stylePgoDynamic = (currentHour >= 6 && currentHour < 19) ? stylePgoDay : stylePgoNight
+    map.mapTypes.set('style_pgo_dynamic', stylePgoDynamic)
+
+
+    map.setMapTypeId(Store.get('map_style'))
+
+    map.addListener('zoom_changed', function () {
+        if (storeZoom === true) {
+            Store.set('zoomLevel', this.getZoom())
+        } else {
+            storeZoom = true
+        }
+
+
+    })
+
+    if (Push._agents.chrome.isSupported()) {
+        createServiceWorkerReceiver()
+    }
+}
