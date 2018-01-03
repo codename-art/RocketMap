6ccf28be649b3e74e57c26f998bdd9e237dca877
codename-art <120071994>
2018-01-03 03:05:28 +0300

    Add weather layer on main page (cherry picked from commit 0fbc587)
    Make weather overlay use relative paths to weather icons
    (cherry picked from commit fa897e3)
    
    Add check if top bar weather placeholder is null
    (cherry picked from commit 9a74098)

--- static/js/map.common.js
+++ static/js/map.common.js
@@ -954,6 +954,18 @@ var StoreOptions = {
         default: false,
         type: StoreTypes.Boolean
     },
+    'showWeatherCells': {
+        default: false,
+        type: StoreTypes.Boolean
+    },
+    'showS2Cells': {
+        default: false,
+        type: StoreTypes.Boolean
+    },
+    'showWeatherAlerts': {
+        default: false,
+        type: StoreTypes.Boolean
+    },
     'playSound': {
         default: false,
         type: StoreTypes.Boolean
@@ -1103,7 +1115,10 @@ var mapData = {
     pokestops: {},
     lurePokemons: {},
     scanned: {},
-    spawnpoints: {}
+    spawnpoints: {},
+    weather: {},
+    s2cells: {},
+    weatherAlerts: {}
 }
 
 // Populated by a JSON request.
